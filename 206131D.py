927022f1103e240cf4000760f57bd503ff169de1
savini <savini1235@gmail.com>
2023-10-15 11:06:28 +0530

    Updated 206131D.py with additional changes

--- 206131D.py
+++ 206131D.py
@@ -1,67 +1,69 @@
-#Activity 1
 class Node:
-	def __init__(self, k, value):
-		self.key = k
-		self.value = value
-		self.next = None
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
 
-class HTable:
-	def __init__(self, m):
-		self.size = m
-		self.table = [None] * m
+    def _hash(self, key):
+        return key % self.size
 
-	def _hash(self, k):
-		return k % self.size
+    def insert(self, key, value):
+        index = self._hash(key)
+        if self.table[index] is None:
+            self.table[index] = Node(key, value)
+        else:
+            current = self.table[index]
+            while current.next:
+                current = current.next
+            current.next = Node(key, value)
 
-#Activity 2
-	def insert(self, k, value):
-		index = self._hash(k)
-		if self.table[index] is None:
-			self.table[index] = Node(k, value)
-		else:
-			current = self.table[index]
-			while current.next:
-				current = current.next
-			current.next = Node(k, value)
+    def search(self, key):
+        index = self._hash(key)
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        return None
 
-#Activity 3
-	def search(self, k):
-		index = self._hash(k)
-		current = self.table[index]
-		while current:
-			if current.key == k:
-				return current.value
-			current = current.next
-		return False
+    def delete(self, key):
+        index = self._hash(key)
+        current = self.table[index]
+        previous = None
+        while current:
+            if current.key == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.table[index] = current.next
+                return
+            previous = current
+            current = current.next
 
-#Activity 4
-	def delete(self, k):
-		index = self._hash(k)
-		current = self.table[index]
-		previous = None
-		while current:
-			if current.key == k:
-				if previous:
-					previous.next = current.next
-				else:
-					self.table[index] = current.next
-				return
-			previous = current
-			current = current.next
+# Create a HashTable instance
+table1 = HashTable(20)
 
-# Activity 5
-table1 = HTable(20)
-table1.insert(206131, "Savini")
-table1.insert(206075, "Senuri")
-table1.insert(206053, "Kalani")
-table1.insert(206083, "Chathumini")
-table1.insert(206104, "Reema")
+# Insert data into the HashTable
+data = {
+    206131: "Savini",
+    206075: "Senuri",
+    206053: "Kalani",
+    206083: "Chathumini",
+    206104: "Reema"
+}
 
-# Activity 6
+for key, value in data.items():
+    table1.insert(key, value)
+
+# Search for a specific key
 index_number = 206131
-if table1.search(index_number):
-    name = table1.search(index_number)
+name = table1.search(index_number)
+if name is not None:
     print("Name found:", name)
 else:
     print("Name not found.")
